bf1028100bfe80652fc73092b05df527c1447029
Nicolas Mayoral <n.mayoral@alumno.um.edu.ar>
2023-10-17 20:27:24 -0300

    env variables setted in app.py and workflow

--- app.py
+++ app.py
@@ -1,38 +1,53 @@
+# Description: This is the main application file for the Flask application.
+
+# Python imports
 import os
 import sys
 import logging
+from dotenv import load_dotenv
 
+# Flask imports
 from flask import Flask
 from flask import jsonify
 
+# OpenTelemetry imports
 from opentelemetry import trace
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
 from opentelemetry.instrumentation.requests import RequestsInstrumentor
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
-
 from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
-
 
 # Initialize Flask
 app = Flask(__name__)
 
-# Load configuration
-# app.config.from_object(Config)
+# Load environment variables
+load_dotenv()
 
-# Set global TracerProvider before instrumenting
-# trace.set_tracer_provider(
-#     TracerProvider(
-#         resource=Resource.create({SERVICE_NAME: 'flask-helloworld'})
-#     )
-# )
+CONNECTION_STRING = os.environ.get('CONNECTION_STRING')
+OTEL_SERVICE_NAME = os.environ.get('OTEL_SERVICE_NAME')
+OTEL_TRACES_SAMPLER_ARG = os.environ.get('OTEL_TRACES_SAMPLER_ARG')
 
+# Load configuration of OpenTelemetry
 tracer_provider = TracerProvider(
-    resource=Resource.create({SERVICE_NAME: 'flask-helloworld'})
+    resource=Resource.create({SERVICE_NAME: OTEL_SERVICE_NAME})
 )
 
 trace.set_tracer_provider(tracer_provider)
+
+# Enable tracing for Flask library
+FlaskInstrumentor().instrument_app(app)
+
+# Enable tracing for requests library
+RequestsInstrumentor().instrument()
+
+trace_exporter = AzureMonitorTraceExporter(
+    connection_string=CONNECTION_STRING
+)
+trace.get_tracer_provider().add_span_processor(
+    BatchSpanProcessor(trace_exporter)
+)
 
 # Configure logging
 # logging.basicConfig(level=logging.DEBUG)
@@ -43,24 +58,13 @@ trace.set_tracer_provider(tracer_provider)
 # app_logger.debug(f"OTEL_SERVICE_NAME: {Config.OTEL_SERVICE_NAME}")
 # app_logger.debug(f"OTEL_TRACES_SAMPLER_ARG: {Config.OTEL_TRACES_SAMPLER_ARG}")
 
-# Enable tracing for Flask library
-FlaskInstrumentor().instrument_app(app)
-
-# Enable tracing for requests library
-RequestsInstrumentor().instrument()
-
-trace_exporter = AzureMonitorTraceExporter(
-    connection_string='InstrumentationKey=c73b01ac-5294-40a4-8fb4-88085453c928;IngestionEndpoint=https://eastus-8.in.applicationinsights.azure.com/;LiveEndpoint=https://eastus.livediagnostics.monitor.azure.com/'
-)
-trace.get_tracer_provider().add_span_processor(
-    BatchSpanProcessor(trace_exporter)
-)
-
 # Import the application views
 @app.route('/')
 def index():
     return jsonify({'message': 'Hello, World!'})
 
 if __name__ == "__main__":
-    # Run the application
+    '''
+    Start the Flask application.
+    '''
     app.run(debug=True, port=5000, host='0.0.0.0')
